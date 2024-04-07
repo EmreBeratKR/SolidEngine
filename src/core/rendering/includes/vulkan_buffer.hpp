bbd31958af961ac16426b14ec5ff6fd778b29bf6
EmreBeratKR <emreberatkrn@gmail.com>
2024-04-08 00:06:32 +0300

    foldering

---
+++ src/core/rendering/includes/vulkan_buffer.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+
+#include <vector>
+#include <string>
+
+
+#include "vulkan_graphic_engine.h"
+#include "vertex.h"
+
+
+namespace Engine::Rendering
+{
+    template<typename T>
+    class VulkanBuffer
+    {
+    private:
+        VkBuffer buffer;
+        VkDeviceMemory memory;
+        std::vector<T> items;
+
+
+    public:
+        VkBuffer GetVkBuffer() { return buffer; }
+        std::size_t GetSize() { return this->items.size(); }
+        void AddItem(T item) { this->items.push_back(item); }
+
+        void AllocateWithUsageFlag(VkBufferUsageFlagBits usageFlag)
+        {
+            VulkanGraphicEngine* vulkan = VulkanGraphicEngine::GetInstance();
+            VkDeviceSize bufferSize = sizeof(items[0]) * items.size();
+            VkBuffer stagingBuffer;
+            VkDeviceMemory stagingBufferMemory;
+            VkDevice device = vulkan->GetLogicalDevice();
+
+            vulkan->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+            void* data;
+            vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+            memcpy(data, items.data(), (size_t)bufferSize);
+            vkUnmapMemory(device, stagingBufferMemory);
+
+            vulkan->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | usageFlag, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, buffer, memory);
+            vulkan->copyBuffer(stagingBuffer, buffer, bufferSize);
+
+            vkDestroyBuffer(device, stagingBuffer, nullptr);
+            vkFreeMemory(device, stagingBufferMemory, nullptr);
+        }
+    };
+
+    class VertexBuffer : public VulkanBuffer<Vertex>
+    {
+    public:
+        void Allocate()
+        {
+            AllocateWithUsageFlag(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+        }
+    };
+
+    class IndexBuffer : public VulkanBuffer<uint32_t>
+    {
+    public:
+        void Allocate()
+        {
+            AllocateWithUsageFlag(VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
+        }
+    };
+}
--- src/core/rendering/includes/vulkan_graphic_engine.fwd.h
+++ src/core/rendering/includes/vulkan_graphic_engine.fwd.h
@@ -4,7 +4,7 @@
 #define GLFW_INCLUDE_VULKAN
 
 
-#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 
 namespace Engine::Rendering
@@ -14,6 +14,7 @@ namespace Engine::Rendering
         public:
             void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
             void createImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+            void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
             void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
             void transitionImageLayout(VkImage image, VkFormat format, VkImageAspectFlags aspectMask, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
             void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
